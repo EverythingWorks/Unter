513a43fdb2102ee152d416b330264f7c2744f75a
arorias <podsiadlo.gj@gmail.com>
2018-04-23 15:53:12 +0200

    Add more login and signup tests

--- rides_handling/tests.py
+++ rides_handling/tests.py
@@ -6,6 +6,7 @@ from .models import Profile
 from django.contrib.auth.models import User
 from .apps import RidesHandlingConfig
 from django.apps import apps
+from django.urls import reverse
 
 class SignupFormTest(TestCase):
     def test_if_signup_form_is_valid(self):
@@ -41,3 +42,30 @@ class RidesHandlingConfigTest(TestCase):
     def test_apps(self):
         self.assertEqual(RidesHandlingConfig.name, 'rides_handling')
         self.assertEqual(apps.get_app_config('rides_handling').name, 'rides_handling')
+    
+
+class ViewTest(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('12345blablasd')
+        user.save()
+
+    def test_user_log_in(self):
+        c = Client()
+        logged_in = c.login(username='testuser', password='12345blablasd')
+        self.assertTrue(logged_in)
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_user_can_log_out(self):
+        c = Client()
+        logged_in = c.login(username='testuser', password='12345blablasd')
+        self.assertTrue(logged_in)
+        response = self.client.get(reverse('logout'), follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_template_get(self):
+        response = self.client.get(reverse('signup'))
+        self.assertTemplateUsed(response, 'signup.html')
+    
+       
