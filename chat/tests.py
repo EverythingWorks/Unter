d08c6e5473ea5d4cde00e9bb98ab04dd77a13ebf
arorias <podsiadlo.gj@gmail.com>
2018-05-31 23:27:24 +0200

    Add basic tests for chat

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.apps import apps
+from .apps import ChatConfig
+from .forms import MessageForm
 
-# Create your tests here.
+class ChatConfigTest(TestCase):
+    def test_apps(self):
+        self.assertEqual(ChatConfig.name, 'chat')
+        self.assertEqual(apps.get_app_config('chat').name, 'chat')
+
+
+class MessageFormTest(TestCase):
+    def test_if_message_form_is_valid(self):
+        good_form = MessageForm(data={'content' : 'blabh blabh'})
+        self.assertTrue(good_form.is_valid())
+
